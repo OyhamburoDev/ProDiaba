3caffd7679e75828e1f9649e763ddb39dccbb207
OyhamburoDev <oyhamburoramiro@gmail.com>
2025-04-05 15:47:39 -0300

    Add initial firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEsdAdVZwpMRHyZFbeSFHzHguzSUdMGS8",
+  authDomain: "prodiaba-e8ca7.firebaseapp.com",
+  projectId: "prodiaba-e8ca7",
+  storageBucket: "prodiaba-e8ca7.firebasestorage.app",
+  messagingSenderId: "1061204911891",
+  appId: "1:1061204911891:web:decc65ddbf2df034bc1ce6",
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { registerRootComponent } from "expo";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
+import "react-native-gesture-handler";
 
 registerRootComponent(() => (
   <ThemeProvider>
